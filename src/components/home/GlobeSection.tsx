de35f4d56c2d7a25bd838e47ac62a020a1765671
kimnerd625 <jihunkim625@yonsei.ac.kr>
2024-02-21 16:20:08 +0900

    ✏️ fix: 커밋 메시지 컨벤션 일관화

--- src/components/home/GlobeSection.tsx
+++ src/components/home/GlobeSection.tsx
@@ -13,20 +13,12 @@ const GlobeSection = () => {
       <main className="relative flex items-center justify-center min-h-screen">
         {isLoaded && (
           <>
-
-            <div className="absolute left-4 sm:left-6 lg:left-48 top-1/2 transform -translate-y-1/2 text-5xl sm:text-6xl lg:text-8xl text-white z-10">
-=======
             <div className="absolute left-48 top-1/2 transform -translate-y-1/2 text-8xl text-white z-10">
-              <h2>ECHO</h2>
-            <div className="absolute right-4 sm:right-6 lg:right-48 top-1/2 transform -translate-y-1/2 text-2xl sm:text-3xl lg:text-4xl text-white z-10 space-y-1 sm:space-y-2 lg:space-y-8 text-right">
-=======
-            <div className="absolute left-4 sm:left-6 lg:left-48 top-1/2 transform -translate-y-1/2 text-5xl sm:text-6xl lg:text-8xl text-white z-10">
               <h2>ECHO</h2>
               <h2>FOR</h2>
               <h2>ECO</h2>
             </div>
-            <div className="absolute right-4 sm:right-6 lg:right-48 top-1/2 transform -translate-y-1/2 text-2xl sm:text-3xl lg:text-4xl text-white z-10 space-y-1 sm:space-y-2 lg:space-y-8 text-right">
->>>>>>> 86e8614 (💄 style: GlobeSection.tsx 반응형 웹 디자인 적용)
+            <div className="absolute right-48 top-1/2 transform -translate-y-1/2 text-4xl text-white z-10 space-y-8 text-right">
               <h2>Uniting Voices</h2>
               <h2>Sharing Acts </h2>
               <h2>for a Greener</h2>
